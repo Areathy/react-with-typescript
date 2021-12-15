ae9f42d2e8b6bd68b8a44e665e5f90dfbd4f77f0
Henrietha <areathy33@gmail.com>
2021-12-15 23:44:51 +0100

    Random data types

--- src/App.tsx
+++ src/App.tsx
@@ -4,28 +4,27 @@ import './App.css';
 
 function App() {
 
-  const [ number, setNumber ] = useState<number | string>(5);
+  const [people, setPeople] useState([
+    {
+      name: "Henrietha Njoku",
+      url: "henrietha.com",
+      age: "16",
+      note: "Allergic to failure"
+    },
+    {
+      name: "Timothy Fabelurin",
+      url: "henrietha.com",
+      age: "16",
+    }
+  ])
 
-  const changeNumber = () => {
-    setNumber("19")
-  }
+  people.map(person => {
+    person.name 
+  })
 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Haallo People</h1>
     </div>
   );
 }
